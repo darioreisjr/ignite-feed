47184e242f09d9fa48c7d1ea0642476c913cb369
Dario Reis <guttyerresjunior1995@gmail.com>
2025-02-10 09:58:25 -0300

    Estrutura da aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
+import { Header } from './components/Header';
 import { Post } from "./Post"
 
+import styles from './App.module.css'
+
 import './global.css'
-import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Dario Reis"
-        content="esse é um post Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque corrupti aperiam dolorum. Minima non, sunt laboriosam repudiandae atque tenetur corrupti laudantium porro dolore ab. Sit, laudantium impedit? Ea, voluptatem illo?"
-      />
-      <Post
-        author="Felipe Cardoso"
-        content="esse é outro post Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque corrupt"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Dario Reis"
+            content="esse é um post Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque corrupti aperiam dolorum. Minima non, sunt laboriosam repudiandae atque tenetur corrupti laudantium porro dolore ab. Sit, laudantium impedit? Ea, voluptatem illo?"
+          />
+          <Post
+            author="Felipe Cardoso"
+            content="esse é outro post Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque corrupt"
+          />
+        </main>
+      </div>
     </div>
   )
 }
