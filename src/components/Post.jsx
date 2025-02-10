d98c5b1d77f3e4d1bcac892cc7a2584281d80989
Dario Reis <guttyerresjunior1995@gmail.com>
2025-02-10 20:47:27 -0300

    Estrutura da aplicação - Componente: Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,24 @@
+import styles from './Post.module.css'
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/darioreisjr.png" />
+          <div className={styles.authorInfo}>
+            <strong>Dario Reis</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title='11 de maio as 08:13' dateTime='2022-05-11 08:13:30'> Publicado ha 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifolio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉 <a href="#">jane.design/doctorcare</a></p>
+        <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+      </div>
+    </article>
+  )
+}
